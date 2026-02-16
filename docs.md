8a06e77b1623ef785d2bd732b1d47951037c430f
Ibrahim Amjad <ibrahim.gfs05@gmail.com>
2026-02-16 12:04:53 +0500

    Day 1 report doc

---
+++ docs.md
@@ -0,0 +1,6 @@
+Day 1 milestone
+
+1   setup github repository
+    structured my physics toolkit
+    added this to my github
+    added an euler integration solver template
---
+++ numerical/Euler.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def euler_solver(f, y0, t0, t_end, dt):
+    """ solve ode using Euler integration method
+
+    parameters :
+    f : function 
+        DIFF eq dy/dt = f(t, y)
+
+    y0 : numpy array
+        initial condition
+
+    t0 : float
+        initial condition
+
+    t_end : float
+        final time
+
+    dt : float
+        time step
+
+    returns :
+        t_values : numpy array
+        y_values : numpy array    
+    """
+    pass
+
